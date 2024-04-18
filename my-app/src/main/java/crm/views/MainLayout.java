ae83908bc2ea76c20c7a7e638e5a939bfa10e967
AslanTabaldiev <tabaldiev0906@gmail.com>
2024-04-19 00:06:38 +0600

    first commit

---
+++ my-app/src/main/java/crm/views/MainLayout.java
@@ -0,0 +1,10 @@
+package crm.views;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+public class MainLayout extends AppLayout {
+
+}
